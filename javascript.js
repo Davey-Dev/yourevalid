fe3fb24d495ef1156f13295c9b6a0d00185c1cbd
Pizzachamp <41555495+Pizzachamp@users.noreply.github.com>
2021-12-07 20:39:24 -0500

    did things

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ var valid = ["Take a deep breath and remind yourself that you are in complete co
              "It's okay to feel unstable. It's okay to disassociate. It's okay to hide from the world. It's okay to need help. It's okay not to be okay. Your mental illness is not a personal failure.",
              "We ourselves feel that what we are doing is just a drop in the ocean. But the ocean would be less because of that missing drop.",
              "Going in one more round when you don't think you can, that's what makes all the difference in your life.",
-             "The world is a highly deceptive world, one that constantly asks you to comment but doesn't really care what you have to say. But your friends and I will care, I’m here to listen.",
+             "The world is a highly deceptive world, one that constantly asks you to comment but doesn't really care what you have to say. But your friends and I will care, I'm here to listen.",
              "You are not defected. You are not weird. You are not a lost cause. You are resilient.",
              "When you know that everything that is happening is only appearing on the screen of consciousness, and that you yourself are the screen on which it all appears, nothing can touch you, harm you or make you afraid.",
              "You are loved. You are capable. You are not weak.",
@@ -29,7 +29,8 @@ var valid = ["Take a deep breath and remind yourself that you are in complete co
              "Positive thoughts are not enough. There have to be positive feelings and positive actions.",
              "Nothing is ever as it seems. What looks bad today, can be a blessing tomorrow.",
              "Patience is the master key to every situation. One must have sympathy for everything, surrender to everything, but at the same time remain patient and forbearing.",
-             "You're gonna be ok, I promise"
+             "You're gonna be ok, I promise.",
+             "You're valid <3"
             ]
 
 function randomNumber(min, max) {  
